3e5155e99299b932dc2018dcff6f81ea02380186
Ubuntu <ubuntu@ip-172-31-12-123.eu-north-1.compute.internal>
2026-08-17 12:25:44 +0000

    Add LabAuditRecord to models.py on server

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from database import Base
 import datetime
 
@@ -12,3 +12,9 @@ class User(Base):
     # --- NEW BILLING FIELDS ---
     has_paid = Column(Boolean, default=False)
     access_valid_until = Column(DateTime, nullable=True)
+class LabAuditRecord(Base):
+    __tablename__ = "lab_audit_records"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    audit_data = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
